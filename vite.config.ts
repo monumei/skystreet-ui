42a699d89ae4d90c7f00c37bcc6ec7971d0ef93d
yonmiru <yonnmiruu@gmail.com>
2025-12-18 11:50:13 +0800

    feat: Separate library build from application build with dedicated Vite config and script.

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'SkyStreetUI',
-      fileName: (format) => `skystreet-ui.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-router-dom': 'ReactRouterDOM',
-        },
-      },
-    },
+    outDir: 'dist-site',
   },
 })
